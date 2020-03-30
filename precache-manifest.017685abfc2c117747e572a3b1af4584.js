562f5c7cca67756c3e4e5a46e13af192ea098b77
Yakovlev Ruslanz <57760502+ZloyBatarejka@users.noreply.github.com>
2020-03-31 00:23:38 +0300

    Updates

---
+++ precache-manifest.017685abfc2c117747e572a3b1af4584.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1ad934d8fe9509a328353803b51222",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4abf02b9041f87535cae",
+    "url": "/static/css/main.7b3f4324.chunk.css"
+  },
+  {
+    "revision": "dad08f107d38cc6355d5",
+    "url": "/static/js/2.a8e9e337.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8e9e337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abf02b9041f87535cae",
+    "url": "/static/js/main.23dee484.chunk.js"
+  },
+  {
+    "revision": "0a30a95bf20210d95cb1",
+    "url": "/static/js/runtime-main.0cf0cd0f.js"
+  },
+  {
+    "revision": "86d3f1dfb4a21be7b2de439290dac24a",
+    "url": "/static/media/flag.86d3f1df.jpg"
+  }
+]);
